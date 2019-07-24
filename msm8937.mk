f2c0118b94a81a0f561dbe7cb4c0c49b8520a026
Stargazer19 <pviraj19@gmail.com>
2019-07-24 19:57:40 +0530

    msm8937-common: Build ThermalController

--- msm8937.mk
+++ msm8937.mk
@@ -362,6 +362,10 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
     android.hardware.thermal@1.0-service \
     thermal.msm8937
+	
+#ThermalController
+PRODUCT_PACKAGES += \
+	ThermalController
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
