b7b3d7e3308d20239adb2cd360a1d015be1eef13
Ojas Sinha <sinhaojas67@gmail.com>
2019-08-07 02:12:38 +0000

    add fluence audio
    Signed-off-by: Ojas Sinha <sinhaojas67@gmail.com>

--- system_prop.mk
+++ system_prop.mk
@@ -3,4 +3,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	ro.vendor.qti.core_ctl_max_cpu=4 \
 	ro.vendor.qti.core_ctl_min_cpu=2
 
-
+#Audio
+PRODUCT_PROPERTY_OVERRIDES += \
+	persist.vendor.audio.fluence.speaker=true \
+	persist.vendor.audio.fluence.voicecall=true \
+	persist.vendor.audio.fluence.voicerec=false \
+	ro.vendor.audio.sdk.fluencetype=fluence
